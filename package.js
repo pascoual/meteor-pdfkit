a1028d62ab94f760d36a440743e5c311960ac1a8
Pascoual <pascoualito@gmail.com>
2014-10-20 14:55:51 +0200

    Fixed: compatibility with Meteor 0.9.X

--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 Package.describe({
-  summary: "PDFKit, the PDF generation library"
+  summary: "PDFKit, the PDF generation library",
+  version: "1.0.3",
+  git: "https://github.com/pascoual/meteor-pdfkit"
 });
 
 Npm.depends({
-  pdfkit: "0.4.3"
+  pdfkit: "0.7.0"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use(['underscore'], 'server');
-
   api.export('PDFDocument');
-
   api.add_files(['pdfkitWrapper.js'], 'server');
 });
