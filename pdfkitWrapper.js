74b3c000de441fe08629189f090ccc8ecd289bf8
Joaquin Diaz <joaquin.diaz@ies.cl>
2015-09-24 20:06:42 -0300

    this should work to save files in the specified directory

--- pdfkitWrapper.js
+++ pdfkitWrapper.js
@@ -1,2 +1,8 @@
 PDFDocument = Npm.require('pdfkit');
 PDFDocument.PX_PER_CM = 28.33;
+
+fs = Npm.require('fs');
+
+PDFDocument.prototype.writeSync = function (filename) {
+  this.pipe(fs.createWriteStream(filename));
+}
